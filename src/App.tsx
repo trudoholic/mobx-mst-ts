b194684994d22a4c37025da5c6e611f315cf2702
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-02-03 01:09:49 +0300

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,23 @@ interface Props {
 }
 
 const App: React.FC<Props> = observer(({ store }) => {
+    const todosValues = Array.from(store.todos.values());
+
     return (
         <div>
             <button onClick={(e) => store.addTodo(uid(), "New Task")}>
                 Add Task
             </button>
-            {values(store.todos).map((todo) => (
-                // @ts-ignore
+            {todosValues.map((todo) => (
                 <div key={todo?.id}>
-                    {/*{JSON.stringify(todo)}*/}
                     <input
                         type="checkbox"
-                        // @ts-ignore
                         checked={todo?.done}
-                        // @ts-ignore
                         onChange={(e) => todo?.toggle()}
                     />
                     <input
                         type="text"
-                        // @ts-ignore
                         value={todo?.name}
-                        // @ts-ignore
                         onChange={(e) => todo?.setName(e.target.value)}
                     />
                 </div>
